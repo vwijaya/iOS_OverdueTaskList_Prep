d86a6b7cf8e709378a1c00b87f3b3a6b8d1079e8
Valerino Wijaya <valerino@gmail.com>
2014-07-02 14:52:13 +0700

    Initial Commit

---
+++ Prereq for Overdue Assignment/VWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWViewController.h
+//  Prereq for Overdue Assignment
+//
+//  Created by Valerino on 7/2/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWViewController : UIViewController
+
+@end
---
+++ Prereq for Overdue Assignment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prereq for Overdue Assignment
+//
+//  Created by Valerino on 7/2/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VWAppDelegate class]));
+    }
+}
